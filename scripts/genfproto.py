92d7c16a05321bbfcd5259bcdfb08ff42acc2344
Duncan Huntsinger <aloginna@cs.utexas.edu>
2019-09-13 13:30:56 -0500

    Skeletal additions

--- scripts/genfproto.py
+++ scripts/genfproto.py
@@ -1,2 +1,19 @@
 
-    
+import scripts.genutils as genutils
+
+class CFunc(object):
+
+    def __init__(self, instr: genutils.Instruction):
+        pass
+
+    def __str__(self):
+        return ''
+
+    def __repr__(self):
+        return str(self)
+
+def genfproto(input_file, output_file):
+    pass
+
+def main():
+    pass

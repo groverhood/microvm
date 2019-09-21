65f5bec9fafa2e391729e1e6af747b62356b8c96
Duncan Huntsinger <aloginna@cs.utexas.edu>
2019-09-20 23:38:59 -0500

    mas now supports comments

--- src/mas/main.c
+++ src/mas/main.c
@@ -31,13 +31,15 @@ static int errc(void);
 
 static mas_error_t* gerr(int which);
 
+static void perr(mas_error_t *err);
+
 static int assemble(const char *fsrc, char **dest, size_t *written, int verbose);
 
 int main(int argc, char *argv[]) {
     const char *invocation = argv[0];
     struct mas_args_t args;
 
-    argparse(argc, argv, &args);
+    argparse(argc - 1, argv + 1, &args);
 
     if (args.help) {
         help();
@@ -49,10 +51,18 @@ int main(int argc, char *argv[]) {
 
         int err = assemble(args.fsrc, &buf, &nbytes, args.verbose);
 
-        FILE *fp = fopen(args.fdest, "w+");
-        fwrite(buf, sizeof(uint8_t), nbytes, fp);
-        fflush(fp);
-        fclose(fp);
+        if (err) {
+            int count = errc();
+
+            for (int err = 0; err < count; err++) {
+                perr(gerr(err));
+            }
+        } else {
+            FILE *fp = fopen(args.fdest, "w+");
+            fwrite(buf, sizeof(uint8_t), nbytes, fp);
+            fflush(fp);
+            fclose(fp);
+        }
     }
     
     return 0;
@@ -60,7 +70,7 @@ int main(int argc, char *argv[]) {
 
 static int argparse(int argc, char *argv[], struct mas_args_t *restrict args) {
     args->fsrc = NULL;
-    args->fdest = NULL;
+    args->fdest = "a.mbn";
     args->verbose = 0;
     args->help = 0;
 
@@ -78,7 +88,7 @@ static int argparse(int argc, char *argv[], struct mas_args_t *restrict args) {
             args->fdest = fdest;
         } else if (!strcmp(arg, "-v") || !strcmp(arg, "--verbose")) {
             args->verbose = 1;
-        } else {
+        } else if (strcmp(arg, "\n")) {
             args->fsrc = arg;
         }
     }
@@ -102,6 +112,8 @@ static struct mas_error {
     int why;
 } errors[MAS_MAX_ERRORC];
 
+#define MAS_ERROR_INVALTOK 1
+
 static int error_count = 0;
 
 static int errc(void) {
@@ -113,12 +125,24 @@ static mas_error_t* gerr(int which) {
     return result;
 }
 
-static int assemble_line(char *line, int verbose, FILE *out);
+#define ERRSTR(x) "\033[0;31m" x "\033[0m"
+
+static void perr(mas_error_t *err) {
+    switch (err->why) {
+        case MAS_ERROR_INVALTOK: {
+            fprintf(stderr, ERRSTR("UNEXPECTED TOKEN: ") "\"%s\" at (%lu, %lu)\n", err->what, err->line, err->col);
+        }
+    }
+}
+
+static int assemble_line(char *line, int verbose, size_t line_nm, FILE *out);
 
 static int assemble(const char *fsrc, char **dest, size_t *written, int verbose) {
     struct stat fsrc_st;
     char *fct = NULL;
     int err = 0;
+    int success = 1;
+    int erroneous = 0;
 
     if (!stat(fsrc, &fsrc_st)) {
         if (verbose) {
@@ -127,20 +151,23 @@ static int assemble(const char *fsrc, char **dest, size_t *written, int verbose)
 
         fct = malloc(fsrc_st.st_size);
         FILE *fp = fopen(fsrc, "r");
-        fread(fct, 1, fsrc_st.st_size, fp);
+        if (fread(fct, 1, fsrc_st.st_size, fp) != fsrc_st.st_size) {
+            success = 0;
+        }
         fclose(fp);
     }
 
-    if (fct) {
+    if (fct && success) {
         FILE *sstream = open_memstream(dest, written);
         char *line_context;
         char *line = strtok_r(fct, "\n", &line_context);
         size_t line_nm = 0;
 
         while (line) {
-            err = err || assemble_line(line, verbose, sstream);
+            err = err || assemble_line(line, verbose, line_nm, sstream);
 
-            if (error_count == MAS_MAX_ERRORC) {
+            erroneous = (error_count == MAS_MAX_ERRORC);
+            if (erroneous) {
                 puts("Too many errors detected, terminating assembly.");
                 break;
             }
@@ -149,17 +176,36 @@ static int assemble(const char *fsrc, char **dest, size_t *written, int verbose)
             line = strtok_r(NULL, "\n", &line_context);
         }
 
-        fflush(sstream);
+        if (!erroneous) {
+            fflush(sstream);
+        }
         fclose(sstream);
         free(fct);
     }
+
+    return err;
 }
 
-static int assemble_line(char *line, int verbose, FILE *out) {
+static int assemble_line(char *line, int verbose, size_t line_nm, FILE *out) {
     char *context;
     char *tok = strtok_r(line, " \t", &context);
 
+    // comment
+    if (*tok == '#') {
+        return 0;
+    }
+
     if (!strcmp(tok, "halt")) {
         putc(M86_INSTR_HALT, out);
+    } else {
+        mas_error_t *err = errors + error_count;
+        err->line = line_nm;
+        err->col  = (size_t)(tok - line);
+        err->what = tok;
+        err->why  = MAS_ERROR_INVALTOK;
+
+        error_count++;
+
+        return 1;
     }
 }
